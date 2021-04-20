c472d5c14d47707bfa59c6656b5c497a53e7f92f
renatorv <renatorv@gmail.com>
2021-04-20 18:27:34 -0300

    primeiro commit

---
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class ScoreCardWidget extends StatelessWidget {
+  const ScoreCardWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(),
+    );
+  }
+}
+//1 hora e 4 minutos
