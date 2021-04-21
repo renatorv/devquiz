b30f07a13290dd9f4031d9f23e429598b136cc24
renato alves <renatorv@gmail.com>
2021-04-21 15:52:15 -0300

    compilado no mac 2

--- lib/home/widgets/app_bar/app_bar_widget.dart
+++ lib/home/widgets/app_bar/app_bar_widget.dart
@@ -1,6 +1,7 @@
 import 'package:devquiz/core/app_gradients.dart';
 import 'package:devquiz/core/app_text_styles.dart';
 import 'package:flutter/material.dart';
+import '../score_card/score_card_widget.dart';
 
 class AppBarWidget extends PreferredSize {
   AppBarWidget()
@@ -8,34 +9,50 @@ class AppBarWidget extends PreferredSize {
           preferredSize: Size.fromHeight(250),
           child: Container(
             height: 250,
-            decoration: BoxDecoration(
-              gradient: AppGradients.linear,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text.rich(
-                    TextSpan(text: 'Olá, ', style: AppTextStyles.title, children: [
-                      TextSpan(
-                        text: 'Renato',
-                        style: AppTextStyles.titleBold,
-                      ),
-                    ]),
+            child: Stack(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  height: 161,
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    gradient: AppGradients.linear,
                   ),
-                  Container(
-                    width: 58,
-                    height: 58,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      image: DecorationImage(
-                        image: NetworkImage('https://avatars.githubusercontent.com/u/17412678?v=4'),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(
+                        TextSpan(
+                          text: 'Olá, ',
+                          style: AppTextStyles.title,
+                          children: [
+                            TextSpan(
+                              text: 'Renato',
+                              style: AppTextStyles.titleBold,
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
+                      Container(
+                        width: 58,
+                        height: 58,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                              'https://avatars.githubusercontent.com/u/17412678?v=4',
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                Align(
+                  alignment: Alignment(0.0, 1.0),
+                  child: ScoreCardWidget(),
+                ),
+              ],
             ),
           ),
         );
